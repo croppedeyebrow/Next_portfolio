2a6a7c9b9fc3b213c40fd876524dae0b061f589a
Croppedeyebrow <lee940706@gmail.com>
2024-05-27 00:49:39 +0900

    1일차

---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+const HeroContent = () => {
+  const handleReload = () => {
+    window.location.reload(); // 페이지를 새로고침
+  };
+
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[14px]">
+            Cross-platform Developer Portfolio
+          </h1>
+        </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.4)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Controlling
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-cyan-600">
+              <br />
+              {""} the efficiency {""}
+            </span>
+            project outcome
+          </span>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          I&apos;m a cross-platform app developer who is struggling to create a
+          point of contact between user convenience and business model.
+        </motion.p>
+
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-4 button-primary text-center text-white cursor-pointer rounded-lg max-w-[300px]"
+          onClick={handleReload}
+        >
+          C.D.C [개발지휘소]에 대해서 <br />더 알아보기
+        </motion.a>
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="logoicon"
+          height={650}
+          width={650}
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default HeroContent;
